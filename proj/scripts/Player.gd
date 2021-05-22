2026f44eec32a603ebdc15667af3d154eafb1608
IronWood <ironwood777.27@gmail.com>
2021-05-22 09:25:58 +0800

    di ko na alam stop muna

--- proj/scripts/Player.gd
+++ proj/scripts/Player.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 export (int) var speed = 1200
 export (int) var jump_speed = -1800
 export (int) var gravity = 4000
+var paper = preload("res://scenes/paper.tscn")
 
 var velocity = Vector2.ZERO
 
@@ -13,8 +14,14 @@ func get_input():
 		velocity.x += speed
 	if Input.is_action_pressed("left"):
 		velocity.x -= speed
+	if Input.is_action_just_pressed("shoot"):
+		var p = paper.instance()
+		p.position = position
+		p.rotation = $rotate.rotation
+		get_parent().add_child(p)
 
 func _physics_process(delta):
+	$rotate.rotation = (get_global_mouse_position() - $rotate.position).angle()
 	get_input()
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
--- proj/scripts/paper.gd
+++ proj/scripts/paper.gd
@@ -1,9 +1,20 @@
 extends KinematicBody2D
 
 
-var velocity = Vector2(250, 250)
+var velocity = Vector2(400, 400)
+
+func _ready():
+	velocity = get_global_mouse_position().normalized() - get_parent().get_node("Player").position.normalized()
+	velocity.normalized()
+	velocity = velocity*400
+	print(velocity)
 
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
 		velocity = velocity.bounce(collision_info.normal)
+
+
+func _on_playerdetect_body_entered(body):
+	#queue_free()
+	pass
